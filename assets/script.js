8721d5cdd1e990748515a709f786bebb32f8734a
Rcurcurato <133296185+Rcurcurato@users.noreply.github.com>
2023-07-14 21:43:59 -0400

    added psuedo code to script.js file

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+//psuedo code:
+//Set up UI with necessary from inputs and sections 
+//to display current and futures weather conditions.
+
+//Implement the functionality to handle city search. 
+
+//Integrate the API to retrieve the future weather forecase
+//for the next 5 days. Parse the response data and display
+//the forecasted information including date, weather icon,
+//temperature, wind speed, and humidity, in the appropriate 
+//section.
+
+//Implement the search history feature. When a user searches
+//for a city, add that city to the search history with local
+//storage or server side database.
+
+//Handle the click event for the cities in the search history.
