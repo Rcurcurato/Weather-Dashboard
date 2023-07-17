7ea0121ca250bfb1acfefbfddfa40bf6334c012e
Rcurcurato <133296185+Rcurcurato@users.noreply.github.com>
2023-07-17 14:02:00 -0400

    added fetch request to script.js

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@
 
 //Implement the functionality to handle city search. 
 
-//Integrate the API to retrieve the future weather forecase
+//Integrate the API to retrieve the future weather forecast
 //for the next 5 days. Parse the response data and display
 //the forecasted information including date, weather icon,
 //temperature, wind speed, and humidity, in the appropriate 
@@ -16,10 +16,43 @@
 
 //Handle the click event for the cities in the search history.
 
+
 var apiKey = "71d1bf2fb629e5895f40848fc4911d1b"
-
 var city;
+var queryURL;
+var searchButtonEl = getElementById("search-btn")
+var cityInputEl = getElementById("city-input")
+var citiesList = getElementById("cities")
+var searchHistoryEl = getElementById("search-history")
+var historyEl = getElementById("history")
+var currentWeatherEl = getElementById("current-weather")
+var futureWeatherEl = getElementById("five-day-forecast")
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + torrington + "&appid=" + 71d1bf2fb629e5895f40848fc4911d1b;
+queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
-fetch(queryURL)
+//add event listener to search button 
+searchButtonEl.addEventListener("click", searchCity);
+//
+function searchCity () {
+    var city = cityInputEl.value.trim();
+
+    if (city = city !== '') {
+        var apiKey = "71d1bf2fb629e5895f40848fc4911d1b";
+        var queryURL =  "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+         
+        fetch(queryURL)
+        .then(response => response.json())
+        .then(data => {
+
+            displayCurrentWeather(data.currentWeatherEl);
+
+            var futureWeather = data.futureWeatherEl;
+
+            addToSearchHistory(city);
+        })
+    
+        .catch(error => {
+            console.error("Error fetching weather data", error);
+        });
+    }
+}
