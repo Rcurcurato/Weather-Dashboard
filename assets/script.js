98c66d7e8e9ec4c0327a0de84cce07907590b272
Rcurcurato <133296185+Rcurcurato@users.noreply.github.com>
2023-07-25 23:07:28 -0400

    added local storage function to save searched cities and display them to page

--- assets/script.js
+++ assets/script.js
@@ -14,33 +14,39 @@
 //for a city, add that city to the search history with local
 //storage or server side database.
 
-//Handle the click event for the cities in the search history.
-
-// var inputEl = document.querySelector('#city-input');
-// var apiKey = "71d1bf2fb629e5895f40848fc4911d1b";
-// var cardEl = document.getElementById('card');
-
-// // Assuming you have the latitude and longitude values available in the variables lat and lon
-// var lat = 40.7128; // Replace with the actual latitude of the desired location
-// var lon = -74.0060; // Replace with the actual longitude of the desired location
-
-// var queryUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-
-// fetch(queryUrl)
-//     .then(function (response) {
-//         return response.json();
-//     })
-//     .then(function (data) {
-//         console.log(data);
-//         // Handle the weather data here, e.g., update the card element with relevant information
-//     });
-
-
-// JavaScript code starts here
 var inputEl = document.querySelector('#city-input');
-var apiKey = "71d1bf2fb629e5895f40848fc4911d1b"; // Replace "YOUR_API_KEY" with your actual OpenWeatherMap API key
+var apiKey = "cf0dfaf24db5645db03554152b5673ad";
 var weatherCardEl = document.getElementById('weather-card');
+var cityList = document.getElementById("history");
+var cities = [];
 
+//saves searched cities to local storage and retrieves data to display
+//it to saved city list on page
+function renderCities() {
+cityList.innerHTML = "";
+
+for (var i = 0; i < cities.length; i++) {
+    var city = cities[i];
+
+    var li = document.createElement("li");
+    li.textContent = city;
+    li.setAttribute("data-index", i);
+
+    cityList.appendChild(li);
+}
+
+}
+
+function storeCities() {
+    localStorage.setItem("cities", JSON.stringify(cities));
+}
+
+var storedCities = JSON.parse(localStorage.getItem("cities"));
+if (storedCities !== null) {
+    cities = storedCities;
+    renderCities();
+}
+//function to fetch weather data from api and display it to page when user searches for a city
 function fetchWeatherData(cityName) {
     var queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
 
@@ -63,9 +69,10 @@ function fetchWeatherData(cityName) {
             var temperatureEl = document.getElementById("temperature");
             var windEl = document.getElementById("wind");
             var humidityEl = document.getElementById("humidity");
-
+            iconEl.innerHTML = `<img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="Weather Icon">`;
+            
             currentCityEl.textContent = data.name;
-            iconEl.textContent = "Weather Icon: " + data.weather[0].icon; // You might want to display an actual icon image instead of the icon code
+            iconEl.textContent = "Weather Icon: " + data.weather[0].icon;
             temperatureEl.textContent = "Temperature: " + data.main.temp + " K";
             windEl.textContent = "Wind Speed: " + data.wind.speed + " m/s";
             humidityEl.textContent = "Humidity: " + data.main.humidity + "%";
@@ -74,10 +81,59 @@ function fetchWeatherData(cityName) {
             console.log("Error fetching weather data:", error);
             alert("Error fetching weather data. Please try again later.");
         });
+
+        //function to fetch future 5 day weather forecast and display it into 5 seperate boxes when user searches for a city
+} function fetchForecastData(cityName) {
+
+    var forecastUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+    console.log("Forecast URL:", forecastUrl);
+
+
+    fetch(forecastUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+
+
+            if (data.cod && data.cod !== 200) {
+                alert("City not found. Please enter a valid city name.");
+                return;
+            }
+
+            var columns = document.querySelectorAll(".column");
+            for (let i = 0; i < 5; i++) {
+                var forecastDate = new Date(data.list[i].dt_txt);
+                var forecastIcon = data.list[i].weather[0].icon;
+                var forecastTemp = data.list[i].main.temp;
+                var forecastWind = data.list[i].wind.speed;
+                var forecastHumidity = data.list[i].main.humidity;
+
+                var columnEl = columns[i];
+                var forecastContent = `
+                <h4>${forecastDate.toLocaleDateString()}</h4>
+                    <div>Weather Icon: <img src="https://openweathermap.org/img/w/${forecastIcon}.png" alt="Weather Icon"></div>
+                    <div>Temperature: ${forecastTemp} K</div>
+                    <div>Wind Speed: ${forecastWind} m/s</div>
+                    <div>Humidity: ${forecastHumidity}%</div>
+                    `;
+                columnEl.innerHTML += forecastContent;
+
+            }
+        })
+        .catch(function (error) {
+            console.log("error fetching forecast data:", error);
+            alert("error fetching forecast data. Please try again later.")
+
+        })
 }
 
 document.getElementById('search-btn').addEventListener('click', function () {
     var cityName = inputEl.value;
     fetchWeatherData(cityName);
+    fetchForecastData(cityName);
+    cities.push(cityName);
+    storeCities();
+    renderCities();
 });
-
